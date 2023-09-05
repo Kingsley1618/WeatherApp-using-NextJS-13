01283cbe5d52af609f45451a82057b78e8a0506c
Okeze Kingsley <okezekingsley18@gmail.com>
2023-09-05 09:56:04 +0100

    new commit

--- src/app/components/Box.js
+++ src/app/components/Box.js
@@ -10,7 +10,7 @@ async function Box({location}) {
     <>
 
     {!weather.message ? <div className="box w-[100%] items-center h-[300px] rounded-2xl max-w-[800px] grid grid-cols-2  justify-center block mx-auto px-3">
-<Image width = {120} height = {120} src = {`https://openweathermap.org/img/w/${weather?.weather[0].icon}.png`} className="block mx-auto"/>
+<Image width = {120} height = {120} src = {`https://openweathermap.org/img/w/${weather?.weather[0].icon}.png`} className="block mx-auto" alt="weatherImg"/>
 
 <div className="block md:ml-0 mx-auto">
     <h1 className='font-normal'>Today</h1>
--- src/app/components/Error.js
+++ src/app/components/Error.js
@@ -3,7 +3,7 @@ import Image from "next/image"
 function Error({error}) {
   return (
     <div>
-        <Image width = {150} height = {150} src="/spongebob.png" className="block mx-auto" />
+        <Image width = {150} height = {150} src="/spongebob.png" className="block mx-auto" alt="errorImg"/>
        <div className = "text-[40px] font-bold"> {error.cod}</div>
         <div className="font-semibold text-[20px]">{error.message}</div>
         </div>
